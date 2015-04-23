dd993a7c22488f3e7ab582be835446b1e72ecd25
luc <luc@imba.(none)>
2015-04-23 14:50:31 +0200

    actionexecutorPickup

--- object_manipulation_actions/include/object_manipulation_actions/actionExecutorPickupObject.h
+++ object_manipulation_actions/include/object_manipulation_actions/actionExecutorPickupObject.h
@@ -1,25 +1,38 @@
 #ifndef ACTION_EXECUTOR_GRASP_OBJECT_H
 #define ACTION_EXECUTOR_GRASP_OBJECT_H
 
-#include "continual_planning_executive/actionExecutorActionlib.hpp"
+#include "continual_planning_executive/actionExecutorInterface.h"
 #include "continual_planning_executive/symbolicState.h"
-#include <tidyup_msgs/GraspObjectAction.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <grasp_provider_msgs/GenerateGraspsAction.h>
 
 namespace object_manipulation_actions
 {
-
-    class ActionExecutorPickupObject : public ActionExecutorActionlib<tidyup_msgs::GraspObjectAction,
-                                                    tidyup_msgs::GraspObjectGoal, tidyup_msgs::GraspObjectResult>
+    class ActionExecutorPickupObject : public continual_planning_executive::ActionExecutorInterface
     {
         public:
-            virtual bool fillGoal(tidyup_msgs::GraspObjectGoal & goal,
-                    const DurativeAction & a, const SymbolicState & current);
 
-            virtual void updateState(const actionlib::SimpleClientGoalState & actionReturnState,
-                    const tidyup_msgs::GraspObjectResult & result,
-                    const DurativeAction & a, SymbolicState & current);
+    	ActionExecutorPickupObject();
+		~ActionExecutorPickupObject();
+
+		virtual void initialize(const std::deque<std::string> & arguments);
+
+		virtual bool canExecute(const DurativeAction & a, const SymbolicState & currentState) const;
+
+		virtual bool executeBlocking(const DurativeAction & a, SymbolicState & currentState);
+
+		virtual void cancelAction();
+
+
+        private:
+
+		std::string action_name_;
+		ros::ServiceClient getPlanningSceneClient_;
+		actionlib::SimpleActionClient<grasp_provider_msgs::GenerateGraspsAction> actionGenerateGrasps_;
     };
 
 };
+
 
 #endif
--- object_manipulation_actions/src/actionExecutorPickupObject.cpp
+++ object_manipulation_actions/src/actionExecutorPickupObject.cpp
@@ -1,6 +1,10 @@
 #include "object_manipulation_actions/actionExecutorPickupObject.h"
+#include <tidyup_utils/stringutil.h>
+#include <symbolic_planning_utils/moveGroupInterface.h>
 #include <pluginlib/class_list_macros.h>
-#include "tidyup_utils/planning_scene_interface.h"
+#include <moveit_msgs/GetPlanningScene.h>
+#include <moveit/move_group/capability_names.h>
+#include <moveit/move_group_interface/move_group.h>
 
 //PLUGINLIB_DECLARE_CLASS(object_manipulation_actions, action_executor_pickup_object,
 //        object_manipulation_actions::ActionExecutorPickupObject,
@@ -10,45 +14,125 @@ PLUGINLIB_EXPORT_CLASS(object_manipulation_actions::ActionExecutorPickupObject, 
 namespace object_manipulation_actions
 {
 
-    bool ActionExecutorPickupObject::fillGoal(tidyup_msgs::GraspObjectGoal & goal,
-            const DurativeAction & a, const SymbolicState & current)
-    {
-        if(!PlanningSceneInterface::instance()->resetPlanningScene())   // FIXME try anyways?
-            ROS_ERROR("%s: PlanningScene reset failed.", __PRETTY_FUNCTION__);
+ActionExecutorPickupObject::ActionExecutorPickupObject() :
+		actionGenerateGrasps_("generate_grasps", true)
+{
+	ros::NodeHandle nh;
+	ROS_INFO("ActionExecutorPickupObject::%s: Waiting for GetPlanningScene "
+			"service server to start.", __func__);
+	getPlanningSceneClient_ = nh.serviceClient<moveit_msgs::GetPlanningScene>(move_group::GET_PLANNING_SCENE_SERVICE_NAME);
+	getPlanningSceneClient_.waitForExistence();
 
-        ROS_ASSERT(a.parameters.size() == 4);
-        string location = a.parameters[0];
-        goal.pickup_object = a.parameters[1];
-        goal.static_object = a.parameters[2];
-        goal.arm = a.parameters[3];
+	ROS_INFO("ActionExecutorPickupObject::%s: Waiting for generate_grasps "
+			"action server to start.", __func__);
+	actionGenerateGrasps_.waitForServer(); // will wait for infinite time
 
-        return true;
-    }
+	ROS_INFO("ActionExecutorInspectLocation::%s: Action client is ready", __func__);
+}
 
-    void ActionExecutorPickupObject::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
-            const tidyup_msgs::GraspObjectResult & result,
-            const DurativeAction & a, SymbolicState & current)
-    {
-        ROS_INFO("PickupObject returned result");
-        ROS_ASSERT(a.parameters.size() == 4);
-        string location = a.parameters[0];
-        string object = a.parameters[1];
-        string static_object = a.parameters[2];
-        string arm = a.parameters[3];
-// TODO: set grasp in symbolic state?       result.grasp;
-        if(actionReturnState == actionlib::SimpleClientGoalState::SUCCEEDED) {
-            ROS_INFO("PickupObject succeeded.");
-            current.setBooleanPredicate("grasped", object + " " + arm, true);
-            current.setBooleanPredicate("on", object + " " + static_object, false);
-        }
-        else
-        {
-            current.setBooleanPredicate("grasp-impossible", object + " " + location + " " +arm, true);
-        }
-        current.setObjectFluent("arm-state", arm, "arm_unknown");
-        current.setBooleanPredicate("searched", location, false);
-        current.setBooleanPredicate("recent-detected-objects", location, false);
-    }
+ActionExecutorPickupObject::~ActionExecutorPickupObject()
+{
+}
 
-};
+void ActionExecutorPickupObject::initialize(const std::deque<std::string> & arguments)
+{
+	ROS_ASSERT(arguments.size() >= 1);
+	action_name_ = arguments[0];
+}
 
+bool ActionExecutorPickupObject::canExecute(const DurativeAction & a, const SymbolicState & currentState) const
+{
+	return a.name == action_name_;
+}
+
+bool ActionExecutorPickupObject::executeBlocking(const DurativeAction & a, SymbolicState & currentState)
+{
+	ROS_ASSERT(a.parameters.size() == 4);
+	std::string movable_obj = a.parameters[0];
+	std::string arm 		= a.parameters[1];
+	std::string table 		= a.parameters[2];
+	std::string mani_loc 	= a.parameters[3];
+
+	moveit_msgs::GetPlanningScene::Request request;
+	moveit_msgs::GetPlanningScene::Response response;
+	request.components.components = moveit_msgs::PlanningSceneComponents::WORLD_OBJECT_GEOMETRY;
+	if (!getPlanningSceneClient_.call(request, response))
+	{
+		ROS_ERROR("ActionExecutorPickupObject::%s: planning scene request failed.", __func__);
+		return false;
+	}
+
+	moveit_msgs::CollisionObject collObj;
+	bool found = false;
+//	ROS_ASSERT(response.scene.world.collision_objects.size() > 0);
+	forEach(const moveit_msgs::CollisionObject & co, response.scene.world.collision_objects)
+	{
+		ROS_DEBUG("ActionExecutorPickupObject::%s: CollisionObject Name: %s", __func__, co.id.c_str());
+		if (movable_obj.compare(co.id) == 0)
+		{
+			found = true;
+			collObj = co;
+			break;
+		}
+	}
+
+	if (!found)
+	{
+		ROS_ERROR("ActionExecutorPickupObject::%s: No collision object found with name %s",
+				__func__, movable_obj.c_str());
+		return false;
+	}
+
+	grasp_provider_msgs::GenerateGraspsGoal goal;
+	goal.collision_object = collObj;
+	std::string eef_name;
+	moveit::planning_interface::MoveGroup* arm_group;
+	if (StringUtil::startsWith(arm, "left_"))
+	{
+		eef_name = "left_gripper";
+		arm_group = symbolic_planning_utils::MoveGroupInterface::getInstance()->getLeftArmGroup();
+	}
+	else if (StringUtil::startsWith(arm, "right_"))
+	{
+		eef_name = "right_gripper";
+		arm_group = symbolic_planning_utils::MoveGroupInterface::getInstance()->getRightArmGroup();
+	}
+	else
+	{
+		ROS_ERROR("ActionExecutorPickupObject::%s: No arm group could be specified.", __func__);
+		return false;
+	}
+	goal.eef_group_name = eef_name;
+	actionGenerateGrasps_.sendGoal(goal);
+
+	// wait for the action to return
+	bool finished_before_timeout = actionGenerateGrasps_.waitForResult(ros::Duration(30.0));
+
+	if (finished_before_timeout)
+	{
+		actionlib::SimpleClientGoalState state = actionGenerateGrasps_.getState();
+		ROS_INFO("Action finished: %s",state.toString().c_str());
+		if(state != actionlib::SimpleClientGoalState::SUCCEEDED)
+		{
+			ROS_ERROR("ActionExecutorPickupObject::%s: Generate grasp action failed.", __func__);
+			return false;
+		}
+		std::vector<moveit_msgs::Grasp> grasps = actionGenerateGrasps_.getResult()->grasps;
+		ROS_DEBUG("ActionExecutorPickupObject::%s: %lu grasps were found.", __func__, grasps.size());
+		moveit::planning_interface::MoveItErrorCode error_code;
+		error_code = arm_group->pick(movable_obj, grasps);
+		ROS_INFO_STREAM("ActionExecutorPickupObject::" << __func__ <<": Pick action returned "
+				<< error_code);
+		return error_code == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+	} else {
+		ROS_ERROR("ActionExecutorPickupObject: Generate grasp action did not finish before the time out.");
+		return false;
+	}
+	return true;
+}
+
+void ActionExecutorPickupObject::cancelAction()
+{
+}
+
+}; // namespace
