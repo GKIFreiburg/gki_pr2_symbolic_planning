d31546863707130bae38b348cd10db6041b488e6
Lanners <lannersluc@gmail.com>
2015-05-12 17:15:34 +0200

    added dummy action inspectObject which only sets predicate object-inspected

---
+++ object_manipulation_actions/include/object_manipulation_actions/actionExecutorInspectObject.h
@@ -0,0 +1,35 @@
+#ifndef ACTION_EXECUTOR_INSPECT_H
+#define ACTION_EXECUTOR_INSPECT_H
+
+#include "continual_planning_executive/actionExecutorInterface.h"
+#include "continual_planning_executive/symbolicState.h"
+
+namespace object_manipulation_actions
+{
+
+    class ActionExecutorInspectObject : public continual_planning_executive::ActionExecutorInterface
+    {
+        public:
+
+    	ActionExecutorInspectObject();
+		~ActionExecutorInspectObject();
+
+		virtual void initialize(const std::deque<std::string> & arguments);
+
+		virtual bool canExecute(const DurativeAction & a, const SymbolicState & currentState) const;
+
+		virtual bool executeBlocking(const DurativeAction & a, SymbolicState & currentState);
+
+		virtual void cancelAction();
+
+        private:
+
+		std::string action_name_;
+		std::string predicate_object_inspected_;
+
+    };
+
+};
+
+#endif
+
---
+++ object_manipulation_actions/src/actionExecutorInspectObject.cpp
@@ -0,0 +1,70 @@
+#include "object_manipulation_actions/actionExecutorInspectObject.h"
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+PLUGINLIB_EXPORT_CLASS(object_manipulation_actions::ActionExecutorInspectObject, continual_planning_executive::ActionExecutorInterface)
+
+namespace object_manipulation_actions
+{
+	ActionExecutorInspectObject::ActionExecutorInspectObject()
+	{
+
+	}
+
+	ActionExecutorInspectObject::~ActionExecutorInspectObject()
+	{
+
+	}
+
+	void ActionExecutorInspectObject::initialize(const std::deque<std::string> & arguments)
+	{
+		ROS_ASSERT(arguments.size() == 2);
+		action_name_ 			 	= arguments[0];		// inspect-object
+		predicate_object_inspected_ = arguments[1];		// object-inspected
+	}
+
+	bool ActionExecutorInspectObject::canExecute(const DurativeAction & a, const SymbolicState & currentState) const
+	{
+		return a.name == action_name_;
+	}
+
+	bool ActionExecutorInspectObject::executeBlocking(const DurativeAction & a, SymbolicState & currentState)
+	{
+		ROS_ASSERT(a.parameters.size() == 2);
+		std::string object   = a.parameters[0];
+		std::string arm 	 = a.parameters[1];
+
+		// TODO: do useful stuff
+
+		ROS_WARN("ActionExecutorInspectObject::%s: Setting (%s, %s, true)", __func__,
+				predicate_object_inspected_.c_str(), object.c_str());
+		// update symbolic state
+		currentState.setBooleanPredicate(predicate_object_inspected_, object, true);
+
+		bool test = false;
+		Predicate pred;
+		pred.parameters.push_back(predicate_object_inspected_);
+		if (!currentState.hasBooleanPredicate(pred, &test))
+			ROS_ERROR("ActionExecutorInspectObject::%s: Could not find predicate %s", __func__,
+					predicate_object_inspected_.c_str());
+
+		return false;
+
+		if (test)
+			ROS_WARN("ActionExecutorInspectObject::%s: predicate was set successfully", __func__);
+		else
+		{
+			ROS_ERROR("ActionExecutorInspectObject::%s: predicate was not set", __func__);
+			return false;
+		}
+
+		return true;
+	}
+
+	void ActionExecutorInspectObject::cancelAction()
+	{
+
+	}
+
+};
+
