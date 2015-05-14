ee64ef7a1d5df599daf51481da0f52bc04377029
Lanners <lannersluc@gmail.com>
2015-05-14 13:18:38 +0200

    fixed bug: first verify if robot in location and then remember location

--- tidyup_state_creators/src/stateCreatorFromPlanningScene.cpp
+++ tidyup_state_creators/src/stateCreatorFromPlanningScene.cpp
@@ -6,7 +6,6 @@
 #include <moveit/move_group/capability_names.h>
 #include <moveit_msgs/GetPlanningScene.h>
 #include <moveit_msgs/PlanningScene.h>
-#include <tf/transform_listener.h>
 
 PLUGINLIB_EXPORT_CLASS(tidyup_state_creators::StateCreatorFromPlanningScene, continual_planning_executive::StateCreator)
 
@@ -33,6 +32,7 @@ namespace tidyup_state_creators
     	initializeTables(state);
 
 
+    	std::set<std::string> objects_to_remove;
     	// delete all movable objects from symbolic state, and re-add the important objects later (from PS)
 		const multimap<string, string> objects = state.getTypedObjects();
 		std::pair<multimap<string, string>::const_iterator, multimap<string, string>::const_iterator> iterators =
@@ -40,7 +40,9 @@ namespace tidyup_state_creators
 		for (multimap<string, string>::const_iterator it = iterators.first; it != iterators.second; it++)
 		{
 			// also predicates with the objects are removed
-			state.removeObject(it->second);
+//			state.removeObject(it->second);
+
+			objects_to_remove.insert(it->second);
 		}
 
     	ROS_DEBUG_STREAM("StateCreatorFromPlanningScene::" << __func__ << ": number of collision objects in planning scene: "
@@ -64,14 +66,53 @@ namespace tidyup_state_creators
                 continue;
             }
     		addObjectToSymbolicState(state, object, "movable_object");
+    		objects_to_remove.erase(object.id);
+
     		// add object-on predicate to object
-    		findMatchingTable(state, planningScene_.world.collision_objects, object);
+    		std::string closest_table;
+    		closest_table = findMatchingTable(planningScene_.world.collision_objects, object);
+
+            if (!closest_table.empty()) // found a matching table
+            {
+                ROS_INFO_STREAM("StateCreatorFromPlanningScene::" << __func__ << ": putting " << object.id << " on " << closest_table);
+                Predicate pOn;
+                pOn.name = "object-on";
+                pOn.parameters.push_back(object.id);
+                pOn.parameters.push_back(closest_table);
+                state.setBooleanPredicate(pOn.name, pOn.parameters, true);
+            }
+            else
+            	ROS_WARN("StateCreatorFromPlanningScene::%s: NO matching Table found for object: %s",
+            			__func__, object.id.c_str());
+
+            // set predicate object-grasped to false, since object-on table means it is not grasped
+            bool value = false;
+            Predicate p_grasped;
+            p_grasped.name = "object-grasped";
+            p_grasped.parameters.push_back(object.id);
+            p_grasped.parameters.push_back("left_arm");
+            if (state.hasBooleanPredicate(p_grasped, &value))
+            {
+            	ROS_WARN("StateCreatorFromPlanningScene::%s: setting predicate %s %s %s to false", __func__,
+            			p_grasped.name.c_str(), p_grasped.parameters[0].c_str(), p_grasped.parameters[1].c_str());
+            	state.setBooleanPredicate(p_grasped.name, p_grasped.parameters, false);
+            }
+            p_grasped.parameters[1] = "right_arm";
+            if (state.hasBooleanPredicate(p_grasped, &value))
+			{
+            	ROS_WARN("StateCreatorFromPlanningScene::%s: setting predicate %s %s %s to false", __func__,
+						p_grasped.name.c_str(), p_grasped.parameters[0].c_str(), p_grasped.parameters[1].c_str());
+				state.setBooleanPredicate(p_grasped.name, p_grasped.parameters, false);
+			}
+
+
 		}
     	// attached objects
     	forEach(const moveit_msgs::AttachedCollisionObject& attachedObject, planningScene_.robot_state.attached_collision_objects)
     	{
     		const moveit_msgs::CollisionObject& object = attachedObject.object;
     		addObjectToSymbolicState(state, object, "movable_object");
+    		objects_to_remove.erase(object.id);
 
             // grasped predicate
             vector<string> params;
@@ -88,13 +129,30 @@ namespace tidyup_state_creators
                 ROS_DEBUG_STREAM("processing attached object " << object.id << " on right_arm.");
                 params[1] = "right_arm";
                 state.setBooleanPredicate("object-grasped", params, true);
-
-//                state.setBooleanPredicate("object-inspected", object.id, true);
             }
             else
             {
                 ROS_ERROR_STREAM("processing attached object " << object.id << " on unknown link.");
             }
+
+            // set predicate object-on to false, since object is no more on the table when grasped
+    		std::string closest_table;
+    		closest_table = findMatchingTable(planningScene_.world.collision_objects, object);
+            if (!closest_table.empty())
+            {
+                Predicate pOn;
+                pOn.name = "object-on";
+                pOn.parameters.push_back(object.id);
+                pOn.parameters.push_back(closest_table);
+                state.setBooleanPredicate(pOn.name, pOn.parameters, false);
+            }
+    	}
+
+    	// remove objects not present in planning scene
+    	for (std::set<std::string>::iterator it = objects_to_remove.begin(); it != objects_to_remove.end(); it++)
+    	{
+    		ROS_INFO("StateCreatorFromPlanningScene::%s: removing object %s from symbolic state", __func__, (*it).c_str());
+    		state.removeObject(*it);
     	}
 
     	return true;
@@ -238,11 +296,11 @@ namespace tidyup_state_creators
     	}
     }
 
-    void StateCreatorFromPlanningScene::findMatchingTable(SymbolicState& currentState,
+    std::string StateCreatorFromPlanningScene::findMatchingTable(
     		const std::vector<moveit_msgs::CollisionObject>& allCos,
     		const moveit_msgs::CollisionObject& co)
     {
-        string closest_table = "closest-table";
+        string closest_table = "";
         double closest_distance = 2.0;
         forEach(const moveit_msgs::CollisionObject& table, allCos)
         {
@@ -255,7 +313,7 @@ namespace tidyup_state_creators
                 if (!extractPoseStampedFromCollisionObject(table, table_pose_stamped))
                 {
                 	ROS_ERROR("StateCreatorFromPlanningScene::%s: table:%s does not have a pose!", __func__, table.id.c_str());
-                	return;
+                	continue;
                 }
 
                 // get the point of co
@@ -263,7 +321,7 @@ namespace tidyup_state_creators
                 if (!extractPoseStampedFromCollisionObject(co, co_pose_stamped))
                 {
                 	ROS_ERROR("StateCreatorFromPlanningScene::%s: object:%s does not have a pose!", __func__, co.id.c_str());
-                	return;
+                	return closest_table;
                 }
 
                 // using only positions of table and collision object
@@ -275,15 +333,16 @@ namespace tidyup_state_creators
                 {
                 	ROS_WARN("StateCreatorFromPlanningScene::%s: table pose in frame: %s and object pose in frame: %s",
                 			__func__, table_pose_stamped.header.frame_id.c_str(), co.header.frame_id.c_str());
+                	ROS_INFO("StateCreatorFromPlanningScene::%s: converting object pose to frame %s", __func__,
+                			table_pose_stamped.header.frame_id.c_str());
 
                 	// transform object pose in table frame
                     geometry_msgs::PoseStamped co_pose_transformed;
-                    tf::TransformListener tf;
                     try {
-                        tf.waitForTransform(table_pose_stamped.header.frame_id, co_pose_stamped.header.frame_id,
+                        tf_.waitForTransform(table_pose_stamped.header.frame_id, co_pose_stamped.header.frame_id,
                         		co_pose_stamped.header.stamp,
                                 ros::Duration(0.5));
-                        tf.transformPose(table_pose_stamped.header.frame_id, co_pose_stamped, co_pose_transformed);
+                        tf_.transformPose(table_pose_stamped.header.frame_id, co_pose_stamped, co_pose_transformed);
                     } catch (tf::TransformException& ex) {
                         ROS_ERROR("StateCreatorFromPlanningScene::%s: %s", __func__, ex.what());
                     }
@@ -315,18 +374,8 @@ namespace tidyup_state_creators
                 }
             }
         }
-        if (closest_table != "closest-table") // found a matching table
-        {
-            ROS_INFO_STREAM("StateCreatorFromPlanningScene::" << __func__ << ": putting " << co.id << " on " << closest_table);
-            Predicate pOn;
-            pOn.name = "object-on";
-            pOn.parameters.push_back(co.id);
-            pOn.parameters.push_back(closest_table);
-            currentState.setBooleanPredicate(pOn.name, pOn.parameters, true);
-        }
-        else
-        	ROS_WARN("StateCreatorFromPlanningScene::%s: NO matching Table found for object: %s",
-        			__func__, co.id.c_str());
+
+        return closest_table;
     }
 };
 
