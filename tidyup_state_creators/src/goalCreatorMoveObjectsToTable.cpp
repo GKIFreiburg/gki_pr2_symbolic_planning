a40f9d44885d26057ff77b2eea4b28347a130513
Lanners <lannersluc@gmail.com>
2015-05-14 15:39:26 +0200

    removed old code from goalcreators

--- tidyup_state_creators/src/goalCreatorMoveObjectsToTable.cpp
+++ tidyup_state_creators/src/goalCreatorMoveObjectsToTable.cpp
@@ -1,14 +1,7 @@
 #include "tidyup_state_creators/goalCreatorMoveObjectsToTable.h"
-#include "tidyup_utils/geometryPoses.h"
-#include "tidyup_utils/stringutil.h"
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
-#include <tf/transform_datatypes.h>
-#include <set>
-#include <std_srvs/Empty.h>
 
-//PLUGINLIB_DECLARE_CLASS(tidyup_state_creators, goal_creator_move_objects_to_table,
-//        tidyup_state_creators::GoalCreatorMoveObjectsToTable, continual_planning_executive::GoalCreator)
 PLUGINLIB_EXPORT_CLASS(tidyup_state_creators::GoalCreatorMoveObjectsToTable, continual_planning_executive::GoalCreator)
 
 namespace tidyup_state_creators
@@ -28,9 +21,7 @@ namespace tidyup_state_creators
 
     bool GoalCreatorMoveObjectsToTable::fillStateAndGoal(SymbolicState & currentState, SymbolicState & goal)
     {
-        //goal.setForEachGoalStatement("manipulation_location", "searched", true);
     	goal.setForEachGoalStatement("manipulation_location", "location-inspected", true);
-        //goal.setForEachGoalStatement("movable_object", "tidy", true);
     	goal.setForEachGoalStatement("movable_object", "object-inspected", true);
     	goal.setForEachGoalStatement("arm", "hand-free", true); // derived predicate
 
--- tidyup_state_creators/src/goalCreatorTidyupInitialize.cpp
+++ tidyup_state_creators/src/goalCreatorTidyupInitialize.cpp
@@ -76,10 +76,6 @@ namespace tidyup_state_creators
             return false;
         }
 
-        // TODO: why these sets? They are filled but never used.
-        std::set<string> rooms;
-        std::set<string> doors;
-        std::set<string> static_objects;
         forEach(const GeometryPoses::NamedPose & np, locations.getPoses())
         {
             const string& location = np.first;
@@ -101,63 +97,12 @@ namespace tidyup_state_creators
             const string& room = nameParts[nameParts.size()-1];
             const string& type = nameParts[0];
             string name = nameParts[0];
-//            for(size_t i = 1; i < nameParts.size()-1; i++)
-//                name += "_" + nameParts[i];
-// commented out by me - resulting in KeyError = 'room'
-//            rooms.insert(room);
-//            currentState.addObject(room, "room");
-//            currentState.setObjectFluent("location-in-room", location, room);
-//
-//            if (StringUtil::startsWith(type, "door"))
-//            {
-//                doors.insert(name);
-//                currentState.addObject(name, "door");
-//
-//                // door_in_location
-//                currentState.setObjectFluent("belongs-to-door", location, name);
-//                currentState.addObject(location, "door_in_location");
-//                goal.addObject(location, "door_in_location");
-//
-//                // also create the matching door_out_location as rotZ by 180 deg
-//                geometry_msgs::PoseStamped outPose = np.second; // copy everything, only switch orientation
-//                tf::Quaternion rot180 = tf::createQuaternionFromYaw(M_PI);
-//                tf::Quaternion locRot;
-//                tf::quaternionMsgToTF(outPose.pose.orientation, locRot);
-//                tf::quaternionTFToMsg(locRot * rot180, outPose.pose.orientation);
-//
-//                string outLocation = location + "_out";
-//                currentState.setObjectFluent("belongs-to-door", outLocation, name);
-//                currentState.addObject(outLocation, "door_out_location");
-//                goal.addObject(outLocation, "door_out_location");
-//
-//                currentState.setNumericalFluent("timestamp", outLocation, outPose.header.stamp.toSec());
-//                currentState.setObjectFluent("frame-id", outLocation, outPose.header.frame_id);
-//                currentState.setNumericalFluent("x", outLocation, outPose.pose.position.x);
-//                currentState.setNumericalFluent("y", outLocation, outPose.pose.position.y);
-//                currentState.setNumericalFluent("z", outLocation, outPose.pose.position.z);
-//                currentState.setNumericalFluent("qx", outLocation, outPose.pose.orientation.x);
-//                currentState.setNumericalFluent("qy", outLocation, outPose.pose.orientation.y);
-//                currentState.setNumericalFluent("qz", outLocation, outPose.pose.orientation.z);
-//                currentState.setNumericalFluent("qw", outLocation, outPose.pose.orientation.w);
-//                //currentState.setObjectFluent("location-in-room", outLocation, room);
-//                currentState.setObjectFluent("location", outLocation, room);
-//            }
-//            else
-            {
-                static_objects.insert(name);
-                //currentState.addObject(name, "static_object");
-                currentState.addObject(name, "table");
-                //currentState.setBooleanPredicate("static-object-at-location", name + " " + location, true);
-                // currentState.setBooleanPredicate("location-near-table", name + " " + location, true);
-                currentState.setBooleanPredicate("location-near-table", location + " " + name, true);
-                currentState.addObject(location, "manipulation_location");
-                goal.addObject(location, "manipulation_location");
-            }
-        }
 
-//		  // Not defined in domain predicates
-//        currentState.setBooleanPredicate("can-grasp", "right_arm", true);
-//        currentState.setBooleanPredicate("can-grasp", "left_arm", true);
+			currentState.addObject(name, "table");
+			currentState.setBooleanPredicate("location-near-table", location + " " + name, true);
+			currentState.addObject(location, "manipulation_location");
+			goal.addObject(location, "manipulation_location");
+        }
 
         currentState.setObjectFluent("arm-state", "right_arm", "arm_unknown");
         currentState.setObjectFluent("arm-state", "left_arm", "arm_unknown");
