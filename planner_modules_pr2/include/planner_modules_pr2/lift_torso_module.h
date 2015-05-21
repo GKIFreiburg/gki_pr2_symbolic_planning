c7bf5ab612bb6140fd58ce3cbdf7df7a0f95e07e
Lanners <lannersluc@gmail.com>
2015-05-21 10:53:49 +0200

    implemented cost module - working

--- planner_modules_pr2/include/planner_modules_pr2/lift_torso_module.h
+++ planner_modules_pr2/include/planner_modules_pr2/lift_torso_module.h
@@ -4,14 +4,48 @@
 #include "tfd_modules/module_api/pddlModuleTypes.h"
 #include <ros/ros.h>
 
+// Distance measured from ground when torso is at minimum (= not lifted)
+const double MIN_TORSO_POSITION = 0.802;
+// Offset between torso_lift_link and head_pan_link (from tf)
+const double OFFSET_TORSO_HEAD = 0.38145;
+// Soft Joint limits for torso_lift_link (from urdf file)
+const double MIN_TORSO_JOINT = 0.00115;
+const double MAX_TORSO_JOINT = 0.325;
+
+const double INFINITE_COST = HUGE_VAL;
+
+
+double table_height_;
+double torso_position_;
+double vdist_head_to_table_;
+double lift_speed_;
+std::string world_frame_;
+
+// set the variable table_height_ and torso_position_
+bool fetchVariablesFromPlanner(const modules::ParameterList & parameterList,
+		modules::numericalFluentCallbackType numericalFluentCallback);
+
+// Compute the real distance that torso needs to be lifted, considering the joint limits of
+// torso_lift_link
+double computeLiftDistance();
+
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-void lift_torso_init(int argc, char** argv);
+void liftTorsoInit(int argc, char** argv);
 
-double lift_torso_cost(const modules::ParameterList & parameterList,
+// Cost Module, computing real costs for lifting torso
+double liftTorsoCost(const modules::ParameterList & parameterList,
+		modules::predicateCallbackType predicateCallback,
+		modules::numericalFluentCallbackType numericalFluentCallback, int relaxed);
+
+// Condition checker Module, check if torso needs to be lifted
+// INFINITE_COST if false or 0 (a value smaller INFINITE_COST) if true
+// INFINITE_COST = HUGE_VAL
+double needToLiftTorso(const modules::ParameterList & parameterList,
 		modules::predicateCallbackType predicateCallback,
 		modules::numericalFluentCallbackType numericalFluentCallback, int relaxed);
 
--- planner_modules_pr2/src/lift_torso_module.cpp
+++ planner_modules_pr2/src/lift_torso_module.cpp
@@ -1,23 +1,112 @@
 #include "planner_modules_pr2/lift_torso_module.h"
 #include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
+#include <tf/transform_listener.h>
 
 //#include <boost/foreach.hpp>
 //#define forEach BOOST_FOREACH
 
-VERIFY_CONDITIONCHECKER_DEF(lift_torso_cost);
+VERIFY_CONDITIONCHECKER_DEF(liftTorsoCost);
+VERIFY_CONDITIONCHECKER_DEF(needToLiftTorso);
 
-void lift_torso_init(int argc, char** argv)
+// ________________________________________________________________________________________________
+bool fetchVariablesFromPlanner(const modules::ParameterList & parameterList,
+		modules::numericalFluentCallbackType numericalFluentCallback)
 {
-    ROS_INFO("%s: Initialized lift_torso Module.", __func__);
+	// get table name from planner interface
+	ROS_ASSERT(parameterList.size() == 1);
+
+	modules::ParameterList table;
+	table.push_back(parameterList[0]);
+
+	modules::ParameterList empty;
+
+	// fetch values from planner
+	modules::NumericalFluentList nfRequest;
+	nfRequest.reserve(2);
+	nfRequest.push_back(modules::NumericalFluent("z", table));
+	nfRequest.push_back(modules::NumericalFluent("torso-position", empty));
+
+    modules::NumericalFluentList* nfRequestP = &nfRequest;
+    if(!numericalFluentCallback(nfRequestP)) {
+        ROS_ERROR("lift_torso_modules::%s: numericalFluentCallback failed.", __func__);
+        return false;
+    }
+
+    // initialize values
+    table_height_ 	= nfRequest[0].value;
+    torso_position_ = nfRequest[1].value;
+
+    ROS_INFO("lift_torso_modules::%s: table height: %lf and torso_position: %lf",
+    		__func__, table_height_, torso_position_);
+
+    return true;
 }
 
-double lift_torso_cost(const modules::ParameterList & parameterList,
+// ________________________________________________________________________________________________
+double computeLiftDistance()
+{
+	// Compute distance between head and table
+	double head_height = MIN_TORSO_POSITION + torso_position_ + OFFSET_TORSO_HEAD;
+	double distance = head_height - table_height_;
+
+	// Compute height that torso needs to be lifted
+	double lift_distance = torso_position_ + vdist_head_to_table_ - distance;
+
+	// Verify that lift_distance does not leave joint bounds, else set to the corresponding limit
+	if (lift_distance < MIN_TORSO_JOINT)
+		lift_distance = MIN_TORSO_JOINT;
+	else if (lift_distance > MAX_TORSO_JOINT)
+		lift_distance = MAX_TORSO_JOINT;
+
+	return lift_distance;
+}
+
+// ________________________________________________________________________________________________
+void liftTorsoInit(int argc, char** argv)
+{
+	ROS_ASSERT(argc == 2);
+
+    ros::NodeHandle nhPriv("~");
+    std::string tfPrefix = tf::getPrefixParam(nhPriv);
+    world_frame_ = tf::resolve(tfPrefix, argv[1]);
+
+    // /continual_planning_executive/vdist_head_to_table)
+    nhPriv.param("/continual_planning_executive/vdist_head_to_table", vdist_head_to_table_, 0.62);
+    // If not defined on param server, take 0.02 m/s which is an arbitrary chosen value
+    nhPriv.param("lift_speed", lift_speed_, 0.02);
+
+   	ROS_INFO_STREAM("lift_torso_modules::" << __func__ << ": param namespace: " << nhPriv.getNamespace() << "\n"
+		"world frame: " << world_frame_ << "\n"
+		"vdist_head_to_table: " << vdist_head_to_table_ << "\n"
+		"lift_speed: " << lift_speed_);
+
+
+    ROS_INFO("lift_torso_modules::%s: Initialized lift_torso Module.", __func__);
+}
+
+// ________________________________________________________________________________________________
+double liftTorsoCost(const modules::ParameterList & parameterList,
 		modules::predicateCallbackType predicateCallback,
 		modules::numericalFluentCallbackType numericalFluentCallback, int relaxed)
 {
-	double cost;
-	cost = 12.5;
-    return cost;
+
+	fetchVariablesFromPlanner(parameterList, numericalFluentCallback);
+
+	double cost = computeLiftDistance();
+
+	return cost/lift_speed_;
 }
+
+// ________________________________________________________________________________________________
+double needToLiftTorso(const modules::ParameterList & parameterList,
+		modules::predicateCallbackType predicateCallback,
+		modules::numericalFluentCallbackType numericalFluentCallback, int relaxed)
+{
+	double cost = 0.0;
+	ROS_ERROR("CALLING %s, cost: %lf", __func__, cost);
+
+	return cost;
+}
+
+
 
