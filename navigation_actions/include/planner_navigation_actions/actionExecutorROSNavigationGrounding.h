26b0273f37784dde1818e52262ad1b8d158fdd71
Lanners <lannersluc@gmail.com>
2015-08-26 15:22:38 +0200

    rewrote the action executor for move actions which is now capable of lifting torso

--- navigation_actions/include/planner_navigation_actions/actionExecutorROSNavigationGrounding.h
+++ navigation_actions/include/planner_navigation_actions/actionExecutorROSNavigationGrounding.h
@@ -1,17 +1,18 @@
 #ifndef ACTION_EXECUTOR_R_O_S_NAVIGATION_GROUNDING_H
 #define ACTION_EXECUTOR_R_O_S_NAVIGATION_GROUNDING_H
 
-#include "continual_planning_executive/actionExecutorActionlib.hpp"
+#include "continual_planning_executive/actionExecutorInterface.h"
 #include "continual_planning_executive/symbolicState.h"
+#include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <moveit/move_group_interface/move_group.h>
 #include <vector>
 #include <utility>
 
 namespace navigation_actions
 {
 
-    class ActionExecutorROSNavigationGrounding : public ActionExecutorActionlib<move_base_msgs::MoveBaseAction
-                                        , move_base_msgs::MoveBaseGoal, move_base_msgs::MoveBaseResult>
+    class ActionExecutorROSNavigationGrounding : public continual_planning_executive::ActionExecutorInterface
     {
         public:
             /**
@@ -22,22 +23,30 @@ namespace navigation_actions
              */
             virtual void initialize(const std::deque<std::string> & arguments);
 
-            virtual bool fillGoal(move_base_msgs::MoveBaseGoal & goal,
-                    const DurativeAction & a, const SymbolicState & current);
+    		virtual bool canExecute(const DurativeAction & a, const SymbolicState & currentState) const;
 
-            virtual void updateState(const actionlib::SimpleClientGoalState & actionReturnState,
-                    const move_base_msgs::MoveBaseResult & result,
-                    const DurativeAction & a, SymbolicState & current);
+    		virtual bool executeBlocking(const DurativeAction & a, SymbolicState & currentState);
+
+    		virtual void cancelAction();
 
         protected:
-            ///< predicates to set/unset for the start location on successful execution
-            std::vector<std::pair<std::string, bool> > _startPredicates;
-
-            ///< predicates to set/unset for the goal location on successful execution
-            std::vector<std::pair<std::string, bool> > _goalPredicates;
-
+            std::string action_name_;
+            std::string action_topic_move_base_;
             std::string predicate_table_inspected_recently_;
             std::string sampled_torso_height_;
+
+            actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>* action_move_base_;
+
+            // also possible to use action client for lifting torso
+            // topic: "torso_controller/position_joint_action"
+            // actionlib::SimpleActionClient<control_msgs::SingleJointPositionAction>* action_lift_torso_;
+
+            // using move group instead of normal action server because move group also checks for collisions
+            moveit::planning_interface::MoveGroup* torso_group_;
+
+            bool executeMoveBase(const geometry_msgs::PoseStamped& target_pose);
+
+            bool executeLiftTorso(const geometry_msgs::PoseStamped& target_pose);
 
     };
 
